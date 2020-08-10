615d276f1cc14bdcf5e2c39c6b090da2edf78c75
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2020-08-10 11:23:51 +0100

    Update config.py

--- base/config.py
+++ base/config.py
@@ -5,10 +5,11 @@ class Config:
     ENV = 'dev'
 
     if ENV == 'dev':
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///db.api' #for development dababase
     else:
 
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///db.api'  #for production database 
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'something secret' #os.environ.get('SECRET_KEY') change later
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
